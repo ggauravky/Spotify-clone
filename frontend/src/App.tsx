f9804a14fc7c1de24a40f49765e1f5ba1366811e
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-06-10 22:43:16 +0530

    initialise project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="text-amber-300">Hi</div>
+      <Button>This is A button</Button>
+    </>
+  )
+}
+
+export default App
