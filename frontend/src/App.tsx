dff1d8ce76425c4a433c3a44a158bc2f8b9d6b4e
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-06-27 11:15:27 +0530

    little bit changes in auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import { Route, Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import authCallback from './pages/auth-callback/authCallback';
 
 function App() {
+
+
+
   return (
     <>
-    <Router>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<authCallback />} />
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<authCallback />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/auth-callback/authCallback.tsx
+++ frontend/src/pages/auth-callback/authCallback.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
 const authCallback = () => {
   return (
     <div>authCallback</div>
   )
 }
+
 
 export default authCallback
